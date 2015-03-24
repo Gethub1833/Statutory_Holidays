9395f5781a8893ab877f3c38e8e460b0621e9d3e
Shane Howearth <shane@shanehowearth.com>
2015-03-24 19:37:54 +1300

    Completed all New Zealand statutory holidays, valid from 1 January 2014

--- Provincial/Westland.py
+++ Provincial/Westland.py
@@ -1,8 +1,10 @@
 __author__ = 'shane'
 '''
-Chatham Islands	30 November	Monday nearest to the actual day
+Westland	Hokitika, Greymouth	1 December	Monday nearest to the actual day (Greymouth)
+Varies (outside Greymouth)
 '''
 from datetime import date, timedelta
+
 
 def get_holiday(year):
     weekday = get_actual(year).weekday()
@@ -17,5 +19,5 @@ def get_holiday(year):
         return get_actual(year) + timedelta(days=8-weekday)
 
 def get_actual(year):
-    NOVEMBER=11
-    return date(year,NOVEMBER,30)
+    DECEMBER=12
+    return date(year,DECEMBER,1)
