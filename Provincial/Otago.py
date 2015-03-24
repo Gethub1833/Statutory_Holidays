e96ee87f0c5d55a82ae3be39261078d33ae3e33a
Shane Howearth <shane@shanehowearth.com>
2015-03-24 20:50:54 +1300

    Changed Otago provincial holiday to move to the previous weekend if it coincides with Easter, this is just a guess at the moment, will look into it properly in the morning

--- Provincial/Otago.py
+++ Provincial/Otago.py
@@ -10,23 +10,12 @@ def get_holiday(year):
     import Easter_Sunday
     # This holiday varies if easter falls on the same weekend
     easter_sunday = Easter_Sunday.get_actual(year)
-    if easter_sunday.month == MARCH:
-        pass
+    if easter_sunday.month == MARCH and easter_sunday.day in range(20, 28):
+        # Easter and Otago anniversary collide
+        return stat_helper.get_nearest_monday(get_actual(year))-timedelta(days=7)
     else:
         return stat_helper.get_nearest_monday(get_actual(year))
 
-def get_holiday(year):
-
-
-    if weekday == 0:
-        # Sunday
-        return get_actual(year) + timedelta(days=1)
-    elif weekday in (1,2,3,4):
-        # Monday, Tuesday, Wednesday, Thursday
-        return get_actual(year) - timedelta(days=weekday-1)
-    else:
-        # Friday, Saturday
-        return get_actual(year) + timedelta(days=8-weekday)
 
 def get_actual(year):
     MARCH=3
