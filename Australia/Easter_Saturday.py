f390e1bc33ae1daaa95ee33a73c5a5eb3bded081
Shane Howearth <shane@shanehowearth.com>
2015-03-28 09:44:02 +1300

    Australian tests done, and National holidays complete

--- Australia/Easter_Saturday.py
+++ Australia/Easter_Saturday.py
@@ -1,1 +1,11 @@
 __author__ = 'shane'
+from datetime import timedelta
+from Easter import Easter_Sunday
+
+
+def get_holiday(year):
+    return get_actual(year)
+
+
+def get_actual(year):
+    return Easter_Sunday.get_actual(year)-timedelta(days=1)
--- unit_tests/Australia_tests.py
+++ unit_tests/Australia_tests.py
@@ -1,14 +1,60 @@
-# -*- coding: iso-8859-15 -*-
 __author__ = 'shane'
 from Australia import Australia_day, Easter_Saturday, ANZAC
 from Christmas_and_New_Year import New_Years_day, Christmas, Boxing_day
 from Easter import Good_Friday, Easter_Monday
 from datetime import date
+
+
+#############
+# Australia #
+#############
+assert Australia_day.get_holiday(2015) == date(2015, 1, 26)
+
+###################
+# Easter Saturday #
+###################
+assert Easter_Saturday.get_holiday(2015) == date(2015, 4, 4)
+
+
+#############
+# ANZAC Day #
+#############
+assert ANZAC.get_actual(2015) == date(2015, 4, 25)
+assert ANZAC.get_holiday(2015) == date(2015, 4, 27)
+
+# Christmas, Boxing Day, and New Years day, and Easter are well tested, so all
+# we need to test is that we can call them properly
+
 #############
 # New Years #
 #############
 assert New_Years_day.get_holiday(2015) == date(2015, 1, 1)
+assert New_Years_day.get_actual(2015) == date(2015, 1, 1)
 
+#############
+# Christmas #
+#############
+assert Christmas.get_holiday(2015) == date(2015, 12, 25)
+assert Christmas.get_actual(2015) == date(2015, 12, 25)
+
+##############
+# Boxing Day #
+##############
+# AKA Proclamation day
+assert Boxing_day.get_holiday(2015) == date(2015, 12, 28)
+assert Boxing_day.get_actual(2015) == date(2015, 12, 26)
+
+###############
+# Good Friday #
+###############
+assert Good_Friday.get_actual(2015) == date(2015, 4, 3)
+assert Good_Friday.get_holiday(2015) == date(2015, 4, 3)
+
+#################
+# Easter Monday #
+#################
+assert Easter_Monday.get_actual(2015) == date(2015, 4, 6)
+assert Easter_Monday.get_holiday(2015) == date(2015, 4, 6)
 '''
 National Public Holidays 2015
 
@@ -24,69 +70,8 @@ Friday 25: December Christmas Day
 Monday 28 December**: Boxing Day
 
 **Substitute for Boxing Day 26 December 2015.
-
-Christmas_and_New_Year Day
-
-Christmas_and_New_Year in Australia falls in mid-summer, rather than mid-winter
-like in the northern hemisphere. It is also the middle of the annual summer
-school holidays. The lead-up to Christmas_and_New_Year Day is a very busy time
-for shopping with Australians buying gifts for their friends and families.
-Shop windows are often full of toys and displays with fake snow, reindeer and
-icicles, even if the temperatures may be 30 C outside. Traditionally,
-Christmas lunch is a gathering for families who eat roast turkey, ham and hot
-plum pudding in the northern hemisphere tradition, although many Australian
-families now opt for an outdoor Aussie-style BBQ.
-
-There is a long-standing tradition for many international visitors to head to
-Sydney's famous Bondi Beach for a picnic on Christmas_and_New_Year Day.
-
-Boxing Day
-
-The Boxing Day public holiday is held the day after Christmas_and_New_Year Day each year. In South Australia it is called Proclamation Day which celebrates the establishment of South Australia as a British province. Sport features highly on this public holiday. The Sydney to Hobart yacht race starts each Boxing Day in Sydney with sailors from around the globe competing. Summer is also cricket season in Australia. The traditional Boxing Day Test Cricket match held at the Melbourne Cricket Ground attracts more than 100,000 spectators. Boxing Day is also the start of the post-Christmas_and_New_Year bargain sale season with crowds of shoppers queuing for shop doors to open in most Australian cities and large towns.
-
-New Year's Day
-
-New Year's Day is celebrated on 1 January each year in all states and
-territories. For many people, New Year's Day is a time to recover from
-New Year's Eve parties the night before. Each major city in Australia holds
-New Year's Eve celebrations, usually accompanied by fireworks and other events,
-such as those held on Sydney Harbour. The Perth Cup, Western Australia's
-premier thoroughbred racing event is held at the Ascot Racecourse on New Year's
-Day annually. People from all around the world join the party at Sullivans Cove,
-on Hobart's waterfront to cheer the new year with crews from the Sydney to
-Hobart yacht race. Many Australians and their families also spend the day
-travelling home at the end of their summer vacation.
-
-Australia Day
-
-Australia Day is Australia's national day held on 26 January annually in all
-states and territories. Australia Day commemorates the founding of the first
-European settlement at Sydney's Port Jackson in 1788. It's an opportunity for
-all Australians to come together to celebrate our country and culture. Many
-people use the day to picnic in a park, have family barbecues or play or watch
-sporting events. In some places, particularly Lake Burley Griffin in Canberra,
-spectacular public fireworks displays are held. In towns and cities,
-citizenship ceremonies are held to welcome immigrants who have been granted
-Australian citizenship. The Australian of the Year Awards are also presented
-by the Prime Minister to acknowledge those who have made an outstanding
-contribution to our country and communities.
-
-Easter
-
-The celebration of Easter has a large impact on Australia for almost everyone regardless of their faith. The Easter long weekend is one of two major Australian holiday periods during the year. School holidays occur around Easter and Australian families often take several days away to enjoy the Autumn weather. The two-week long Sydney Royal Easter Show includes the four-day Easter weekend. In Ulladulla, New South Wales, the Italian community holds a blessing of the fishing fleet over the Easter weekend commemorating St Peter as the patron saint of the fishermen on the New South Wales coast. In Victoria, the Rip Curl Pro held every Easter at Bells Beach on the Great Ocean Road has become an annual pilgrimage for the world’s best surfers.
-
-Anzac Day
-
-ANZAC Day is the day on which Australia remembers those who served the country during war time. It began to commemorate the Australian and New Zealand Army Corps (ANZAC) soldiers who landed at Gallipoli in Turkey in World War I. In cities and towns across the country memorial services, parades, marches and reunions of past and current military personnel are held. The largest are at the Australian War Memorial in Canberra and Shrine of Remembrance in Melbourne. After the ceremonies, many people head to Returned Services League (RSL) clubs and play a gambling game called ‘two-up’. The game is usually illegal in most Australian states, but ANZAC Day is one day when the authorities don’t seem to mind. An Australian Football League (AFL) game played at the Melbourne Cricket Ground (MCG) between rivals Collingwood and Essendon on ANZAC Day draws the largest crowd of the year other than the Grand Final.
-
-Labour Day
-
-The Labour Day public holiday varies considerably between the various states and territories. Labour Day, also known as Eight Hours Day in Tasmania and May Day in the Northern Territory, commemorates the granting of the eight-hour working day for Australians. It is always on a Monday, creating a three-day weekend. Many people use the Labour Day holiday to relax, spend time with friends or family, play or watch sport, and enjoy traditional Aussie barbecues. Some people plan a getaway to the coast, beaches, mountains or countryside for picnics, bush-walking or camping. In Brisbane, sometimes commemorative marches or parades are held, although this can depend on the state government in office at the time.
-
-Queen’s Birthday
-
-Australia celebrates the Queen's Birthday because it is a member of the Commonwealth of Nations, with the monarch of the United Kingdom being head of state. In all states and territories except Western Australia, the Queen's Birthday is celebrated on the second Monday in June. Some well-known sporting events are held on this day including an Australian Football League game played at the Melbourne Cricket Ground (MCG) between the Collingwood Magpies and the Melbourne Demons. It is also traditionally seen as the opening weekend of the snow season in Australia. Western Australia celebrates the Queen's Birthday on the last Monday of September or the first Monday of October coinciding with the spring school holidays and the Perth Royal Show.
-
+'''
+'''
 State and Territory Public Holidays 2015
 
 Aside from the national public holidays each state and territory of Australia also declares its individual public holidays.
